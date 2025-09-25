b9fb7919a71b3a5112c69ed5655e36d066ae9427
bveitchd225 <bveitch@glenbrook225.org>
2025-09-25 07:32:39 -0500

    Added MouseMotionListener

--- GBSGame.java
+++ GBSGame.java
@@ -1,20 +1,29 @@
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class GBSGame extends JPanel implements Runnable, KeyListener {
+public class GBSGame extends JPanel implements Runnable, KeyListener, MouseListener, MouseMotionListener {
 
     private Thread gameThread;
     private JFrame f;
 
     private int FPS;
+    private double maxFrameTime;
     private int WIDTH;
     private int HEIGHT;
+
+    private BufferedImage buffer;
+    private Graphics screen;
 
     private int lastReportedFrameRate = -1;
     private int[] reportedFrameRates = new int[100];
@@ -22,18 +31,28 @@ public class GBSGame extends JPanel implements Runnable, KeyListener {
 
     private static ArrayList<String> keys = new ArrayList<>();
     private static ArrayList<String> keyEvents = new ArrayList<>();
+
+    private static int lastReportedMouseX = -1;
+    private static int lastReportedMouseY = -1;
     
     public void setResolution(int w, int h) {
         WIDTH = w;
         HEIGHT = h;
+        buffer = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+        screen = buffer.getGraphics();
     }
 
     public void setFrameRate(int f) {
         FPS = f;
+        maxFrameTime = 1000000000.0 / f;
     }
 
     public int getActiveFrameRate() {
-        return lastReportedFrameRate;
+        return (int) Math.min(lastReportedFrameRate, 1/(maxFrameTime/1000000000));
+    }
+
+    public double getAverageFrameTime() {
+        return 1000.0/lastReportedFrameRate;
     }
 
     public void createWindow() {
@@ -42,11 +61,14 @@ public class GBSGame extends JPanel implements Runnable, KeyListener {
         }
         f = new JFrame();
         f.add(this);
+        f.setTitle("Game");
         this.setPreferredSize(new Dimension(WIDTH,HEIGHT));
         f.pack();
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setVisible(true);
         f.addKeyListener(this);
+        this.addMouseListener(this);
+        this.addMouseMotionListener(this);
         f.setFocusable(true);
         startGameThread();
     }
@@ -65,29 +87,72 @@ public class GBSGame extends JPanel implements Runnable, KeyListener {
         double lastTime = System.nanoTime();
         long currentTime = 0;
 
-        double lastFrame = System.nanoTime();
+        // double lastFrame = System.nanoTime();
+        double start = System.nanoTime();
+        double after = System.nanoTime();
+        double frameTime = 1000000000/FPS;
 
         while (gameThread != null) {
-            currentTime = System.nanoTime();
+            // currentTime = System.nanoTime();
 
-            delta += (currentTime - lastTime) / drawInterval;
-            lastTime = currentTime;
+            // delta += (currentTime - lastTime) / drawInterval;
+            // lastTime = currentTime;
 
-            if (delta >= 1) {
-                reportedFrameRates[rollingIndex%reportedFrameRates.length] = (int) (1 / ((System.nanoTime() - lastFrame)/1000000000));
-                rollingIndex++;
-                if (rollingIndex > reportedFrameRates.length) {
-                    rollingIndex = 0;
-                    int sum = 0;
-                    for (int n: reportedFrameRates) {
-                        sum += n;
-                    }
-                    lastReportedFrameRate = sum / reportedFrameRates.length;
+            // if (delta >= 1) {
+                
+            //     // reportedFrameRates[rollingIndex] = (int) (1.0 / ((System.nanoTime() - lastFrame)/1000000000));
+            //     // lastFrame = System.nanoTime();
+            //     // rollingIndex++;
+            //     // if (rollingIndex > reportedFrameRates.length-1) {
+            //     //     rollingIndex = 0;
+            //     //     int sum = 0;
+            //     //     for (int n: reportedFrameRates) {
+            //     //         sum += n;
+            //     //     }
+            //     //     lastReportedFrameRate = sum / reportedFrameRates.length;
+            //     // }
+            //     double start = System.nanoTime();
+            //     update(delta/FPS);
+            //     repaint();
+            //     System.out.println(System.nanoTime() - start);
+            //     delta=0; 
+            // }
+
+            double lastStart = start;
+            start = System.nanoTime();
+            update((start - lastStart)/1000000000);
+            draw(screen);
+            after = System.nanoTime();
+            repaint();
+            frameTime = after - start;
+            // System.out.println(frameTime/1000000);
+
+            reportedFrameRates[rollingIndex] = (int) (1.0 / (frameTime/1000000000));
+            
+        
+
+            if (frameTime < maxFrameTime) {
+                reportedFrameRates[rollingIndex] = (int) (1.0 / (frameTime/1000000000));
+                // wait until we're ready
+                try {
+                    // System.out.println("Waiting: " + (int) (maxFrameTime - frameTime));
+                    Thread.sleep((long) (maxFrameTime - frameTime)/1000000);
+                } catch (InterruptedException e) {
+                    
                 }
-                lastFrame = System.nanoTime();
-                update(delta/FPS);
-                repaint();
-                delta=0; 
+            }
+            else {
+                reportedFrameRates[rollingIndex] = (int) (1.0 / (frameTime/1000000000));
+            }
+
+            rollingIndex++;
+            if (rollingIndex > reportedFrameRates.length-1) {
+                rollingIndex = 0;
+                int sum = 0;
+                for (int n: reportedFrameRates) {
+                    sum += n;
+                }
+                lastReportedFrameRate = sum / reportedFrameRates.length;
             }
             
         }
@@ -106,7 +171,9 @@ public class GBSGame extends JPanel implements Runnable, KeyListener {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        draw(g);
+        g.setColor(Color.WHITE);
+        g.fillRect(0,0,WIDTH, HEIGHT);
+        g.drawImage(buffer,0,0,null);
     }
 
     public static boolean keyDown(String key) {
@@ -133,6 +200,14 @@ public class GBSGame extends JPanel implements Runnable, KeyListener {
         return keyPressedBool;
     }
 
+    public static int getMouseX() {
+        return lastReportedMouseX;
+    }
+
+    public static int getMouseY() {
+        return lastReportedMouseY;
+    }
+
     @Override
     public void keyTyped(KeyEvent e) {
        
@@ -156,5 +231,45 @@ public class GBSGame extends JPanel implements Runnable, KeyListener {
         while (keyEvents.contains(key)) {
             keyEvents.remove(key);
         }
+    }
+
+    public void onMouseClick(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        onMouseClick(e);
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        lastReportedMouseX = e.getX();
+        lastReportedMouseY = e.getY();
     }
 }
