484a2fedcb4b09f59bb175c5587837193841bf13
bveitchd225 <bveitch@glenbrook225.org>
2025-09-25 07:34:35 -0500

    Large Rework

--- Main.java
+++ Main.java
@@ -1,79 +1,90 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.util.ArrayList;
+
+import java.awt.event.MouseEvent;
 
 public class Main extends GBSGame {
 
-    public static final int SCREEN_WIDTH = 2048;
+    public static final int SCREEN_WIDTH = 1024;
     public static final int SCREEN_HEIGHT = 1024;
 
-    ArrayList<Ball> balls;
-
-    private double[] reportedFrameTimes = new double[100];
-    private int rollingIndex = 0;
+    ArrayList<Particle> particles;
+    QuadTree quadTree;
+    boolean viewQuadTree;
 
     public Main() {
-        balls = new ArrayList<>();
-        for (int i = 0; i < 7500; i++) {
-            balls.add( new Ball() );
+        particles = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            particles.add(new Particle());
         }
-        // balls.add(new Ball(128, 256, 50, 100, 0));
-        // balls.add(new Ball(128+256, 256, 30, -100, 0));
+        viewQuadTree = false;
     }
 
     @Override
     public void update(double dt) {
-        double start = System.nanoTime();
-        for (int i = 0; i < balls.size(); i++) {
-            Ball b = balls.get(i);
-            for (int j = i+1; j < balls.size(); j++) {
-                Ball o = balls.get(j);
+
+        if (GBSGame.keyPressed("q")) {
+            viewQuadTree = !viewQuadTree;
+        }
+
+        // Update Particles
+        for (Particle b : particles) {
+            b.update(dt);
+        }
+
+        // Build Quad Tree
+        quadTree = new QuadTree(new Rectangle(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT), 4);
+        for (Particle b : particles) {
+            quadTree.add(b);
+        }
+
+        // Check Collisions
+        for (int i = 0; i < particles.size(); i++) {
+            Particle b = particles.get(i);
+            for (Particle o : quadTree.query(
+                    new Rectangle((int) b.getX(), (int) b.getY(), (int) b.getRadius() * 2, (int) b.getRadius() * 2))) {
+                if (b == o) {
+                    continue;
+                }
                 b.collidesWith(o);
             }
         }
-        reportedFrameTimes[rollingIndex] = (double) (System.nanoTime() - start)/1000000;
-        rollingIndex++;
-        if (rollingIndex > reportedFrameTimes.length-1) {
-            rollingIndex=0;
-            double sum = 0;
-            for (double n: reportedFrameTimes) {
-                sum += n;
-            }
-            System.out.println(sum / reportedFrameTimes.length);
-        }
-        for (Ball b: balls) {
-            b.update(dt);
-        }
-        // double speedA = balls.get(0).getSpeed();
-        // double speedB = balls.get(1).getSpeed();
-        // double massA = balls.get(0).getMass();
-        // double massB = balls.get(1).getMass();
-        // double kinA = 0.5 * massA * speedA * speedA/1000;
-        // double kinB = 0.5 * massB * speedB * speedB/1000;
-        // System.out.println(kinA + kinB);
+    }
+
+    @Override
+    public void onMouseClick(MouseEvent e) {
+        particles.add(new Particle(e.getX(), e.getY(), 5, 0, 0));
     }
 
     @Override
     public void draw(Graphics g) {
-        for (Ball b: balls) {
+
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+        // Draw Particles
+        for (int i = 0; i < particles.size(); i++) {
+            Particle b = particles.get(i);
             b.draw(g);
         }
+
+        // Draw Quad Tree
+        if (viewQuadTree) {
+            g.setColor(Color.black);
+            quadTree.draw(g);
+        }
         g.setColor(Color.BLACK);
-        g.fillRect(10,10,60,20);
+        g.fillRect(10, 10, 100, 16 * 1 + 4);
         g.setColor(Color.WHITE);
         g.drawString("FPS: " + getActiveFrameRate(), 15, 25);
     }
 
-
-
-
-
-
-
     public static void main(String[] args) {
         Main m = new Main();
         m.setResolution(SCREEN_WIDTH, SCREEN_HEIGHT);
-        m.setFrameRate(1200);
+        m.setFrameRate(480);
         m.createWindow();
     }
 }
